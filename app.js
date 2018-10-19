10bcfec1eade971ba7afab24c074477979533e4a
Graham-Stein <graham@guavoaviation.co.uk>
2018-10-19 15:58:33 +0100

    Add ul adding single li and failing to get another one to append

--- app.js
+++ app.js
@@ -5,27 +5,30 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 const handleForm = function (event) {
-    console.log(event);
   event.preventDefault();
   console.log(event);
-  // const newList = document.createElement('ul');
-  const newParagraph = document.createElement('p');
-    newParagraph.textContent = `Fish is: ${ this.common_name.value },
-     aka ${ this.scientific_name.value }
-     environment(s): ${ this.environment.value }`;
-    const fishList = document.querySelector('#fish-list');
-    fishList.appendChild(newParagraph);
+  // ############ const newList = document.createElement('ul');
+  // const newParagraph = document.createElement('p');
+  //   newParagraph.textContent = `Fish is: ${ this.common_name.value },
+  //    aka ${ this.scientific_name.value }
+  //    environment(s): ${ this.environment.value }`;
+  //   const fishList = document.querySelector('#fish-list');
+  //   fishList.appendChild(newParagraph);
+
+  const newListItem = document.createElement('li');
+  // console.log(this.input.value);
+    newListItem.textContent = `Fish commonly known as: ${ this.common_name.value }`;
+    const fishList = document.querySelector('#fish-ul');
+    fishList.appendChild(newListItem);
+
+  // const newListItem = document.createElement('li');
+  //   newListItem.textContent = `Scientific name: ${ this.scientific_name.value }`;
+  //   const fishList = document.querySelector('#fish-ul');
+  //   fishList.appendChild(newListItem);
+  //
+  // const newListItem = document.createElement('li');
+  //   newListItem.textContent = `Environment: ${ this.environment.value }`;
+  //   const fishList = document.querySelector('#fish-ul');
+  //   fishList.appendChild(newListItem);
 
 };
-
-
-
-// const handleForm = function (event) {
-//   event.preventDefault();
-//   const newParagraph = document.createElement('p');
-//   newParagraph.textContent = `Title is: ${ this.title.value },
-//    by ${ this.author.value }
-//    in category ${ this.category.value }`;
-//   const readingList = document.querySelector('#reading-list');
-//   readingList.appendChild(newParagraph);
-// };
